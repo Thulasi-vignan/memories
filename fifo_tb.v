19d3ac4c2dc87c68a4492c76e32dee2f3dab248e
Thulasi-vignan <thulasivignanbolisetty@gmail.com>
2025-02-15 00:50:42 +0530

    fifo_tb.v

--- fifo_tb.v
+++ fifo_tb.v
@@ -20,7 +20,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module fifo_tb(
+module sync_fifo_tb#(parameter DATA_WIDTH = 8, DEPTH = 16)();
+reg clk ,rst,rd_en,wr_en;
+reg [DATA_WIDTH - 1 :0]data_in;
+wire full , empty;
+wire [DATA_WIDTH - 1 : 0]data_out;
 
-    );
+sync_fifo #(.DATA_WIDTH(DATA_WIDTH),.DEPTH(DEPTH)) uut (.clk(clk),.rst(rst),.rd_en(rd_en),.wr_en(wr_en),.data_in(data_in),.full(full),.empty(empty),.data_out(data_out));
+always#5 clk = ~clk;
+
+initial begin 
+clk = 0;
+
+rst = 1 ;wr_en = 0 ; rd_en = 0 ; data_in = 0 ;
+#10 rst = 0;
+
+repeat(5)begin 
+@(posedge clk)
+    wr_en = 1 ; rd_en = 0 ;
+    data_in = $random %256;
+   end 
+ wr_en = 0 ;
+ 
+ repeat(5)begin 
+ @(posedge clk)
+ wr_en = 0; rd_en = 1;
+ end 
+ rd_en = 0;
+ #10 $finish;
+ end    
+
 endmodule
+
